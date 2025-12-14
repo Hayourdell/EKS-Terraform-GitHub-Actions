086c031fdddc0a92acd445f8abf57378fe051935
Ubuntu <ubuntu@ip-172-31-24-140.ec2.internal>
2025-12-14 17:59:08 +0000

    Updated backend to my S3 bucket

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,20 +1,9 @@
 terraform {
-  required_version = "~> 1.9.3"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.49.0"
-    }
-  }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
-    region         = "us-east-1"
+    bucket         = "ayodele-terraform-backend"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
     encrypt        = true
   }
-}
-
-provider "aws" {
-  region = var.aws-region
 }
